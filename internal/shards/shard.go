b42b3bcdad8f45c4ea774f0bc99f835e5ac4bfb8
tdadadavid <davidtofunmidada@gmail.com>
2026-03-15 01:34:41 +0100

    feat(shard): add tickers for shard cleanup
    Signed-off-by: tdadadavid <davidtofunmidada@gmail.com>

--- internal/shards/shard.go
+++ internal/shards/shard.go
@@ -2,6 +2,7 @@ package shards
 
 import (
 	"context"
+	"fmt"
 	"kvshard/internal/commands"
 	"kvshard/internal/store"
 )
@@ -21,14 +22,31 @@ func NewShard(id int) *Shard {
 func (s *Shard) start(ctx context.Context) {
 	// each shard will have its own event-loop managing its own state
 	// no shared state between shards.
+
+	//TODO: implement keys cleanup when I implement TTL
+	//TODO: clean-up time should be configurable
+	// ticker := time.NewTicker(1 * time.Second)
+	// defer ticker.Stop()
+
+
 	for {
 		select {
 		case <-ctx.Done():
 			// stop shard if parent ctx is cancelled
 			// do cleanup here foreach shard
+			s.cleanup()
+			return
+		// case <-ticker.C:
+		// 	// cleanup expired keys every T durations
+		// 	s.cleanup()
 		default:
+			continue
 		}
 	}
+}
+
+func (s *Shard) cleanup() {
+	fmt.Println("not implemented")
 }
 
 func (s *Shard) Execute(cmd *commands.Command) (*commands.Result, error) {
